fd4f22090e4ca30146cdbba5e8a747c326fab02a
Kumsal Obuz <iam@denizkumsal.com>
2017-02-06 09:09:51 -0500

    cleans the file for future composition logic

--- src/Products.js
+++ src/Products.js
@@ -3,22 +3,19 @@ import {ListGroup} from 'react-bootstrap'
 import Product from './Product'
 import Utils from './Utils'
 
-//console.log(Utils.RawIngredients()) console.log(Utils.ImmediateBuildList())
 
 export default class Products extends Component {
 
     render() {
         let products = Utils.filterByDate(this.props.dateRange)
-/*
+
         if (this.props.ingredients.length){
-            console.log(this.props.ingredients.length)
-            products = Utils.filterByIngredients(products, this.props.ingredients)
-Utils.RawIngredients()
-        }*/
+            //products = Utils.filterByIngredients(products, this.props.ingredients)
+        }
 
 
         products = products.map(product => <Product key={product.name} data={product}/>)
-
+console.log(products.length)
         return (
             <ListGroup>
                 {products}
