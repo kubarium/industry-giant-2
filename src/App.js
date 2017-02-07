4e0383deb548f67d4f3a5db2ceb6aa0134184dc5
Kumsal Obuz <iam@denizkumsal.com>
2017-02-07 09:29:04 -0500

    simplifies component communication

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Products from './Products'
 import Ingredients from './Ingredients'
+import Utils from './Utils'
 
 import Slider, {Handle} from 'rc-slider'
 import Tooltip from 'rc-tooltip'
@@ -32,12 +33,43 @@ export default class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      ingredients: [],
-      date: 1970
+      date: 1970,
+      products: []
     }
   }
-  onDateChange = (date) => this.setState({date})
-  onIngredientsChange = (ingredients) => this.setState({ingredients})
+  onDateChange = (date) => {
+    this.setState({date})
+
+    this.filterProducts(Object.assign({}, {
+      ...this.refs.ingredients.state
+    }, {
+      date
+    }, {sik: false}))
+  }
+  onIngredientsChange = (ingredients) => this.filterProducts(Object.assign({}, {
+    ingredients
+  }, {
+    date: this.state.date
+  }, {sik: true}))
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextState.date === this.state.date
+  }
+
+  filterProducts = (filter) => {
+    console.log(filter)
+
+    this.setState({
+      products: Utils.filterByDate(filter.date)
+    })
+    /*
+
+    if (this.props.filter.ingredients.length) {
+      products = Utils.filterByIngredients(products, this.props.filter.ingredients)
+      // products = products.filter(product =>
+      // Utils.breakdownToRawIngredients(product))
+    }*/
+  }
 
   render() {
     return (
@@ -49,7 +81,6 @@ export default class App extends Component {
           <Row className="filter">
             <Col xs={12} sm={12} lg={10} lgOffset={1} md={10} mdOffset={1}><Slider
               ref="date"
-              step={1}
               min={1900}
               max={1970}
               marks={marks}
@@ -59,13 +90,11 @@ export default class App extends Component {
           </Row>
           <Row className="filter">
             <Col xs={12} sm={12} lg={10} lgOffset={1} md={10} mdOffset={1}>
-              <Ingredients onChange={this.onIngredientsChange}/>
+              <Ingredients ref="ingredients" onChange={this.onIngredientsChange}/>
             </Col>
           </Row>
           <Row>
-            <Col xs={12} sm={12} lg={10} lgOffset={1} md={10} mdOffset={1}><Products
-              ingredients={this.state.ingredients}
-              dateRange={[1900,this.state.date]}/></Col>
+            <Col xs={12} sm={12} lg={10} lgOffset={1} md={10} mdOffset={1}><Products products={this.state.products}/></Col>
           </Row>
         </Grid>
       </div>
