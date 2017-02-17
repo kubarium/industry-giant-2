8c1244a2a422ab9ea21cb4612c89e1291f9c822f
kubarium <iam@denizkumsal.com>
2017-02-17 15:41:25 -0500

    cleans file

--- src/Products.js
+++ src/Products.js
@@ -1,32 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import { ListGroup } from 'react-bootstrap'
+import {ListGroup} from 'react-bootstrap'
 import Product from './Product'
 import Utils from './Utils'
-import { store } from './Store'
+import {store} from './Store'
 
 export default class Products extends Component {
-
-    componentDidMount() {
-        store.subscribe(() => this.setState({products:store.getState().products}))
-    }
     constructor(props) {
         super(props)
         this.state = {
-            products:store.getState().products
+            products: store
+                .getState()
+                .products
         }
     }
+    componentDidMount() {
+        store.subscribe(() => this.setState({
+            products: store
+                .getState()
+                .products
+        }))
+    }
 
-    sortByTotalProfit = (a,b) => {
-        return a.totalProfit < b.totalProfit ? 1 : a.totalProfit > b.totalProfit ? -1 : 0
+    sortByTotalProfit = (a, b) => {
+        return a.totalProfit < b.totalProfit
+            ? 1
+            : a.totalProfit > b.totalProfit
+                ? -1
+                : 0
     }
     render() {
 
-        let products = Utils.FilterByIngredients(store.getState().ingredients).filter(product => product.date <= store.getState().date).sort(this.sortByTotalProfit)
+        let products = Utils
+            .FilterByIngredients(store.getState().ingredients)
+            .filter(product => product.date <= store.getState().date)
+            .sort(this.sortByTotalProfit)
         return (
             <ListGroup>
-              { products
-                    .map(product => <Product key={ product.name } product={ product } />) }
+                {products.map(product => <Product key={product.name} product={product}/>)}
             </ListGroup>
         )
     }
