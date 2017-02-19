397c57b858fa629c01d26d7a530d3a4e53a969d7
Kumsal Obuz <iam@denizkumsal.com>
2017-02-18 23:09:52 -0500

    more complete initial data

--- src/Store.js
+++ src/Store.js
@@ -3,5 +3,7 @@ import products from './data.json'
 import reducers from './Reducers';
 
 export const store = createStore(reducers, {
-    products,ingredients: [],date: 1970
+
+
+    products:localStorage["products"] ? JSON.parse(localStorage["products"]) : products, ingredients: [], stores:["Grocery", "Toys", "Home Appliances", "Furniture", "Clothing", "Home Hardware", "Sports", "Electronics", "Gas Station", "Car Dealer"], date: 2019
 })
