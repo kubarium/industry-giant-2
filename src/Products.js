bbb9f780025e87010aa521f9b4295331892f42fe
Kumsal Obuz <iam@denizkumsal.com>
2017-02-18 23:11:31 -0500

    listens to more filters and does proper sorting

--- src/Products.js
+++ src/Products.js
@@ -1,9 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import {ListGroup} from 'react-bootstrap'
+import { ListGroup } from 'react-bootstrap'
 import Product from './Product'
 import Utils from './Utils'
-import {store} from './Store'
+import { store } from './Store'
 
 export default class Products extends Component {
     constructor(props) {
@@ -22,22 +22,33 @@ export default class Products extends Component {
         }))
     }
 
+
     sortByTotalProfit = (a, b) => {
         return a.totalProfit < b.totalProfit
             ? 1
             : a.totalProfit > b.totalProfit
                 ? -1
                 : 0
-    }
-    render() {
+    } /*
+    sortByStore = (a,b) =>{
+        return a.soldAt
+    }*/
 
-        let products = Utils
-            .FilterByIngredients(store.getState().ingredients)
+    render() {
+        //console.log(Utils.GetProduct("Digital Decoder"))
+        //console.log(Utils.BreakdownToRawIngredients(Utils.FullCompositionList("Digital Decoder")))
+        let products = Utils.FilterByIngredients(store.getState().ingredients)
+            .filter(product => product.merchandisable === true)
             .filter(product => product.date <= store.getState().date)
-            .sort(this.sortByTotalProfit)
+
+        if(store.getState().stores)
+            products = products.filter(product => store.getState().stores.includes(product.soldAt))
+        
+        products.sort(this.sortByTotalProfit)
+
         return (
             <ListGroup>
-                {products.map(product => <Product key={product.name} product={product}/>)}
+              { products.map(product => <Product key={ product.name } product={ product } />) }
             </ListGroup>
         )
     }
