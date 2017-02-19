e473d87d0883d4fb1d8044f36f96b256d1bf93ac
Kumsal Obuz <iam@denizkumsal.com>
2017-02-18 23:12:32 -0500

    implements sold at store events

--- src/Reducers.js
+++ src/Reducers.js
@@ -1,36 +1,45 @@
 import ActionTypes from './ActionTypes';
 
 const reducers = (state, action) => {
-    let ingredients = []
+    //let ingredients = []
 
     switch (action.type) {
         case ActionTypes.TOGGLE_INGREDIENT:
-            ingredients = state.ingredients
-            const index = ingredients.indexOf(action.ingredient)
-            index > -1 ? ingredients.splice(index, 1) : ingredients.push(action.ingredient)
+            let ingredients = state.ingredients
+            const ingredientIndex = ingredients.indexOf(action.ingredient)
+            ingredientIndex > -1 ? ingredients.splice(ingredientIndex, 1) : ingredients.push(action.ingredient)
 
             return Object.assign({},
                 state, {
                     ingredients
                 })
         case ActionTypes.DATE_CHANGE:
-            return Object.assign({}, 
+            return Object.assign({},
                 state
-            , {
-                date: action.date
-            })
-        case ActionTypes.DEMAND_CHANGE:
-            let products = state.products            
-            let product = products[action.product.index]
-            product.demand = action.product.demand
-            product.totalProfit = product.profit * action.product.demand 
+                , {
+                    date: action.date
+                })
+        case ActionTypes.STORE_CHANGE:
+            let stores = state.stores
+            const storeIndex = stores.indexOf(action.store)
+            storeIndex > -1 ? stores.splice(storeIndex, 1) : stores.push(action.store)
 
-            
-            return Object.assign({}, 
+            return Object.assign({},
+                state, {
+                    stores
+                })
+        case ActionTypes.DEMAND_CHANGE:
+            let products = state.products
+            let product = products[action.product.index]
+            product.demand = parseInt(action.product.demand, 10)
+            product.totalProfit = product.profit * action.product.demand
+            localStorage["products"] = JSON.stringify(products)
+
+            return Object.assign({},
                 state
-            , {
-                products
-            })
+                , {
+                    products
+                })
         default:
             return state
     }
