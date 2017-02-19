27056073617b8832e80ccfa602e4a82ba923f418
Kumsal Obuz <iam@denizkumsal.com>
2017-02-18 23:09:25 -0500

    adds sold at store action type

--- src/ActionTypes.js
+++ src/ActionTypes.js
@@ -2,7 +2,8 @@ const ActionTypes = {
     DEMAND_CHANGE: 1,
     PRICE_CHANGE: 2,
     TOGGLE_INGREDIENT:3,
-    DATE_CHANGE:4
+    DATE_CHANGE:4,
+    STORE_CHANGE:5
 }
 
 export default ActionTypes
