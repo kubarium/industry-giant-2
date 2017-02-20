20f8b465656b0c4c803c366483f66db86dc8ac1b
Kumsal Obuz <iam@denizkumsal.com>
2017-02-19 20:02:23 -0500

    cleans file

--- src/Store.js
+++ src/Store.js
@@ -3,7 +3,9 @@ import products from './data.json'
 import reducers from './Reducers';
 
 export const store = createStore(reducers, {
-
-
-    products:localStorage["products"] ? JSON.parse(localStorage["products"]) : products, ingredients: [], stores:["Grocery", "Toys", "Home Appliances", "Furniture", "Clothing", "Home Hardware", "Sports", "Electronics", "Gas Station", "Car Dealer"], date: 2019
+    products: localStorage["products"] ? JSON.parse(localStorage["products"]) : products,
+    ingredients: [],
+    sort: "name",
+    stores: ["Grocery", "Toys", "Home Appliances", "Furniture", "Clothing", "Home Hardware", "Sports", "Electronics", "Gas Station", "Car Dealer"],
+    date: 2019
 })
