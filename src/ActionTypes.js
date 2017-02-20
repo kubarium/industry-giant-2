bf10d18f88f2f82c898e65b524c2a9dd9ed57a6a
Kumsal Obuz <iam@denizkumsal.com>
2017-02-19 20:00:04 -0500

    adds sorting and price adjustment types

--- src/ActionTypes.js
+++ src/ActionTypes.js
@@ -3,7 +3,9 @@ const ActionTypes = {
     PRICE_CHANGE: 2,
     TOGGLE_INGREDIENT:3,
     DATE_CHANGE:4,
-    STORE_CHANGE:5
+    STORE_CHANGE:5,
+    SORT_CHANGE:6,
+    ADJUST_PRICE:7
 }
 
 export default ActionTypes
