8502bcc28dfc91c862ab8831a38a826cb54e7c14
Kumsal Obuz <iam@denizkumsal.com>
2017-02-24 00:08:34 -0500

    new store and data

--- src/Store.js
+++ src/Store.js
@@ -6,6 +6,6 @@ export const store = createStore(reducers, {
     products: localStorage["products"] ? JSON.parse(localStorage["products"]) : products,
     ingredients: [],
     sort: "name",
-    stores: ["Grocery", "Toys", "Home Appliances", "Furniture", "Clothing", "Home Hardware", "Sports", "Electronics", "Gas Station", "Car Dealer"],
+    stores: ["Grocery", "Toys", "Home Appliances", "Furniture", "Clothing", "Home Hardware", "Sports", "Electronics", "Instruments", "Gas Station", "Car Dealer"],
     date: 2019
 })
